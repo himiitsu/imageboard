4a6f148fc8e61fc151e0221b88ad9bd7c50c25e4
mig <dts.msk@gmail.com>
2020-02-18 20:36:32 +0300

    Поддержка PostgreSQL

--- src/main/java/com/project/imageboard/dao/ThreadsDao.java
+++ src/main/java/com/project/imageboard/dao/ThreadsDao.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 @Repository
 public interface ThreadsDao extends JpaRepository<MessageThread, Integer> {
-    @Query(nativeQuery = true, value = "SELECT DISTINCT message_thread_id FROM imageboard.message_thread_messages" +
-            " GROUP BY message_thread_id ORDER BY MAX(messages_current_id) DESC;")
+    @Query(nativeQuery = true, value = "SELECT t.message_thread_id FROM\n" +
+            "        (SELECT DISTINCT message_thread_id, MAX(messages_current_id) AS max FROM message_thread_messages\n" +
+            "        GROUP BY message_thread_id)t\n" +
+            "ORDER BY t.max DESC")
     List<Integer> sortThreads();
 }
